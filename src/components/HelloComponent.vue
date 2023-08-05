a187c76d30ff10820d49a5576b87c00bf9443316
AUTUMN <krshlee22@naver.com>
2023-08-05 18:26:35 +0900

    Feat: Add store

--- src/components/HelloComponent.vue
+++ src/components/HelloComponent.vue
@@ -2,6 +2,12 @@
   <h1>Hello</h1>
 </template>
 
+<script>
+export default {
+  
+}
+</script>
+
 <style scoped lang="scss">
 $color: orange;
 h1 {
---
+++ src/store/message.js
@@ -0,0 +1,11 @@
+export default {
+  // 모듈로 사용하기 위함 
+  namespaced: true,
+  // 데이터는 함수
+  state() {
+    return {}
+  },
+  getters: {},
+  mutations: {},
+  actions: {}
+}
